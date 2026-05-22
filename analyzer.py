9e926e14ea34315efe6935cf344f2287edc36e25
Emperor Munkki <blackcurrant404@users.noreply.github.com>
2026-05-22 17:41:49 +0300

    add basic log parcing and counters

--- analyzer.py
+++ analyzer.py
@@ -2,8 +2,17 @@ def main():
     read_log()
 
 def read_log():
+        Fail_counter = 0
+        Accepted_counter = 0
+
         with open("sample_auth.log") as new_file:
               for line in new_file:
-                    print(line, end="")
+                    line = line.split(" ")
+                    if line[0] == "Accepted":
+                          Accepted_counter += 1
+                    elif line[0] == "Failed":
+                          Fail_counter += 1
+                    else:
+                        raise ValueError         
 
 main()
