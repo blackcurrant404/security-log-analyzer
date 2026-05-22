dd9bf6a994e4736ccde6f93fe9c769a8e7ed9139
Emperor Munkki <blackcurrant404@users.noreply.github.com>
2026-05-22 18:16:03 +0300

    add log analysis report generation

--- analyzer.py
+++ analyzer.py
@@ -2,17 +2,26 @@ def main():
     read_log()
 
 def read_log():
-        Fail_counter = 0
-        Accepted_counter = 0
+    Fail_counter = 0
+    Accepted_counter = 0
 
-        with open("sample_auth.log") as new_file:
-              for line in new_file:
-                    line = line.split(" ")
-                    if line[0] == "Accepted":
-                          Accepted_counter += 1
-                    elif line[0] == "Failed":
-                          Fail_counter += 1
-                    else:
-                        raise ValueError         
+    with open("sample_auth.log") as new_file:
+        for line in new_file:
+            line = line.split(" ")
+            if line[0] == "Accepted":
+                    Accepted_counter += 1
+            elif line[0] == "Failed":
+                        Fail_counter += 1
+            else:
+                raise ValueError     
+
+        print_report(Fail_counter, Accepted_counter)  
+
+                    
+def print_report(failed: int, accepted: int):
+    with open("results.txt", "w") as new_file:
+        new_file.write(f"LOG ANALYZER RESULTS:\n\n")        
+        new_file.write(f"Failed attempts = {failed}\n")
+        new_file.write(f"Accepted attempts = {accepted}\n")
 
 main()
