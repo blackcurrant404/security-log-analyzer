56813fe01f9dc55ef1f050e9dfa0d95da79e0d0f
Emperor Munkki <blackcurrant404@users.noreply.github.com>
2026-05-23 11:38:54 +0300

    add threat level breakdown to report

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,7 @@ import os
 def main():
     fails, accepts, ip_dict = read_log()
     suspicions_list = ip_analysis(ip_dict)
-    print_report(fails, accepts, ip_dict)  
+    print_report(fails, accepts, suspicions_list)  
 
 
 def read_log():
@@ -49,10 +49,31 @@ def ip_analysis(ip_dict):
     return suspicions_dict
         
 
-def print_report(failed_total: int, accepted_total: int, ip_dict: dict):
+def print_report(failed_total: int, accepted_total: int, suspicious_dict: dict):
     with open("results.txt", "w") as new_file:
         new_file.write(f"LOG ANALYZER RESULTS:\n\n")        
         new_file.write(f"Failed attempts = {failed_total}\n")
-        new_file.write(f"Accepted attempts = {accepted_total}\n")
+        new_file.write(f"Accepted attempts = {accepted_total}\n\n\n")
 
+        high_threat_list = []
+        medium_threat_list = []
+        low_threat_list = []
+
+        for ip, threat_level in suspicious_dict.items():
+            if threat_level == "HIGH-THREAT":
+                high_threat_list.append(ip)
+            elif threat_level == "MEDIUM-THREAT":
+                medium_threat_list.append(ip)
+            elif threat_level == "LOW-THREAT":
+                low_threat_list.append(ip)
+
+        new_file.write("HIGH_THREAT:\n") 
+        for ip2 in high_threat_list:
+            new_file.write(f"{ip2}\n")
+        new_file.write("MEDIUM_THREAT:\n") 
+        for ip2 in medium_threat_list:
+            new_file.write(f"{ip2}\n")
+        new_file.write("LOW_THREAT:\n") 
+        for ip2 in low_threat_list:
+            new_file.write(f"{ip2}\n")
 main()
