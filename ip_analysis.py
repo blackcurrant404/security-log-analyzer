a114d65d232811da01efd4c6551177f6af5390dc
Emperor Munkki <blackcurrant404@users.noreply.github.com>
2026-05-27 23:15:28 +0300

    change ip analysis logic to match new ip-attempt tracking

--- ip_analysis.py
+++ ip_analysis.py
@@ -1,13 +1,29 @@
 def analyze(ip_dict):
-    suspicions_dict = {}
-    attempt_sum = 0
-    for ip in ip_dict:
-        attempt_sum = ip_dict[ip]["accepted"] - ip_dict[ip]["failed"]
-        if attempt_sum < -2:
-            suspicions_dict[ip] = "HIGH-THREAT"
-        elif attempt_sum < -1:
-            suspicions_dict[ip] = "MEDIUM-THREAT"
-        elif attempt_sum < 0:
-             suspicions_dict[ip] = "LOW-THREAT"
+    threats = {"high-threat": [], "medium-threat": [], "low-threat": [], "no-threat": []}
+    for ip, items in ip_dict.items():
+        fail_amount = 0
+        threat_level = 0
+        for attempt in items["attempts"]:
+    
+            if attempt == "accepted":
+                fail_amount = 0
+            else:
+                fail_amount += 1
+            
+            if fail_amount > 3 and threat_level < 1:
+                threat_level = 1
+            elif fail_amount > 5 and threat_level < 2: 
+                threat_level = 2
+            elif fail_amount > 7 and threat_level < 3:
+                threat_level = 3
+
+        if threat_level == 3:
+            threats["high-threat"].append(ip)
+        elif threat_level == 2:
+            threats["medium-threat"].append(ip)
+        elif threat_level == 1:
+            threats["low-threat"].append(ip)
+        elif threat_level == 0:
+            threats["no-threat"].append(ip) 
              
-    return suspicions_dict
+    return threats
